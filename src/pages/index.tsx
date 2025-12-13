89c54a11def79275bd8632d78ae61f8da2721dc8
Roberto Melo <rvmelo@inovahu.com>
2025-12-13 17:48:27 -0300

    feat: adiciona form de login

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,1 @@
-import { ChatInput } from '@/components/Inputs/ChatInput'
-import { DefaultInput } from '@/components/Inputs/DefaultInput'
-import { ModalInput } from '@/components/Inputs/ModalInput'
-
-export default function Home() {
-  return (
-    <div>
-      <h1>Next.js</h1>
-      <button>Click me</button>
-      <DefaultInput placeholder="Placeholder" />
-      <ModalInput placeholder="Placeholder" />
-      <ChatInput placeholder="Placeholder" />
-    </div>
-  )
-}
+export { default } from './login'
