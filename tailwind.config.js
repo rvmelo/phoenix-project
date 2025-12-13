b374c72e56c2e598ae8b610913706a3aa5a15a1b
Roberto Melo <rvmelo@inovahu.com>
2025-12-13 07:36:50 -0300

    feat: tipografia
    adiciona tipografia e fontes padrão

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,7 +18,14 @@ module.exports = {
       white: '#FFFFFF',
     },
     extend: {
-      fontSize: {},
+      fontSize: {
+        t1: ['0.875rem', { lineHeight: '1rem', fontWeight: '400' }],
+        t2: ['0.875rem', { lineHeight: '1rem', fontWeight: '600' }],
+        t3: ['1rem', { lineHeight: '1.5rem', fontWeight: '500' }],
+        s1: ['1.25rem', { lineHeight: '1rem', fontWeight: '700' }],
+        s2: ['1.5rem', { lineHeight: '2rem', fontWeight: '700' }],
+        s3: ['1.5rem', { lineHeight: '2rem', fontWeight: '400' }],
+      },
       fontFamily: {
         sans: ['Montserrat', ...defaultTheme.fontFamily.sans],
         grotesk: ['Space Grotesk', ...defaultTheme.fontFamily.sans],
