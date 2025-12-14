60b77a3f157328c59d35ea27434a6ad946de6353
Roberto Melo <rvmelo@inovahu.com>
2025-12-14 17:05:33 -0300

    feat: monta estrutura de grid para telas restantes

--- src/pages/chat/index.tsx
+++ src/pages/chat/index.tsx
@@ -4,8 +4,15 @@ import { UserTopBar } from '@/components/UserTopBar'
 export default function Chat() {
   return (
     <div>
-      <UserSideBar />
-      <UserTopBar sectionTitle="Chat & Assistente Virtual" />
+      <>
+        <UserSideBar />
+        <UserTopBar sectionTitle="Chat & Assistente Virtual" />
+        <div className="grid min-h-screen grid-cols-[9.375rem_1fr] grid-rows-[auto_1fr]">
+          <aside className="row-span-2"></aside>
+          <header className="h-[5.5rem] w-full"></header>
+          <main></main>
+        </div>
+      </>
     </div>
   )
 }
--- src/pages/simulator/index.tsx
+++ src/pages/simulator/index.tsx
@@ -4,8 +4,15 @@ import { UserTopBar } from '@/components/UserTopBar'
 export default function Simulator() {
   return (
     <div>
-      <UserSideBar />
-      <UserTopBar sectionTitle="Simulador de Planos" />
+      <>
+        <UserSideBar />
+        <UserTopBar sectionTitle="Simulador de Planos" />
+        <div className="grid min-h-screen grid-cols-[9.375rem_1fr] grid-rows-[auto_1fr]">
+          <aside className="row-span-2"></aside>
+          <header className="h-[5.5rem] w-full"></header>
+          <main></main>
+        </div>
+      </>
     </div>
   )
 }
